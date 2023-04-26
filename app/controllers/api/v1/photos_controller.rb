3a484316f17bcf63e6b1ce84fede6cbf167d558f
rory jackson <roryjamesjackson@hotmail.com>
2023-04-26 11:35:43 +0100

    redsign complete

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -22,7 +22,7 @@ module Api
         end
         @url = Cloudinary::Utils.cloudinary_url(@photo.photo.key)
 
-        @url.sub!("/image/upload/", "/image/upload/production/")
+        @url.sub!("/image/upload/", "/image/upload/development/")
 
         url = "https://celebrity-face-detection.p.rapidapi.com/"
         headers = {
